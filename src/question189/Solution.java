2bc47878e6ef712ba35f3cfc35c38210fc698aec
ceoicac <ceoicac@163.com>
2017-10-31 22:24:01 +0800

    198_1

--- src/question189/Solution.java
+++ src/question189/Solution.java
@@ -1,5 +1,12 @@
 package question189;
 
+/**
+ * Rotate an array of n elements to the right by k steps.
+ * For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+ * @author user
+ * 先将前n-k个数字旋转，然后旋转第n-k个到第n个的数字，最后整体旋转即可
+ */
+
 public class Solution {
     public void rotate(int[] nums, int k) {
         if(k <= 0){
